e7403749a9923b04837fd0acba22d67422491a59
Romanullah karimi <romanullahhamdard@gmail.com>
2023-03-29 23:23:12 -0400

    the basic of solar system is ready and deployed

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+      solarSystem solar=new solarSystem();
+
+      solar.moon="123";
+      solar.sun="123";
+      solar.planet="3";
+    }
+}
